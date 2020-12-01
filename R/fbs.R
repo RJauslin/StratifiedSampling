116f41aec404157b0d02186df9f5a30655f6650f
JAUSLIN Raphaël <raphael.jauslin@unine.ch>
2020-12-01 08:58:58 +0100

    change in fbs.

--- R/fbs.R
+++ R/fbs.R
@@ -5,7 +5,7 @@
 #' This function implements the method proposed by Hasler and Tille (2019). It should be used for selecting a sample for highly stratified population.
 #'
 #' @param X matrix of auxiliary variables.
-#' @param Xcat matrix of categorical variables.
+#' @param strata matrix of categorical variables.
 #' @param pik vector of inclusion probabilities.
 #'
 #' @details 
@@ -21,10 +21,10 @@
 #' x2 <- rgamma(N,4,25)
 #'
 #' strata <- as.matrix(rep(1:n,each = N/n))
+#' strata <- rep(1:n,each = N/n)
 #'
 #' pik <- sampling::inclusionprobastrata(strata,rep(1,n))
 #' X <- as.matrix(cbind(matrix(c(x1,x2),ncol = 2)))
-#' A <- cbind(X,disjMatrix(as.matrix(strata)))
 #' 
 #' s <- fbs(X,strata,pik)
 #' 
@@ -34,9 +34,9 @@
 #' \emph{Computational Statistics and Data Analysis}, 74, 81-94
 #' 
 #' @export
-fbs <- function(X,Xcat,pik){
+fbs <- function(X,strata,pik){
   
-  H <- as.numeric(ncat(Xcat))
+  H <- as.numeric(ncat(as.matrix(strata)))
   pik_tmp <- pik
   EPS <- 1e-8
   
@@ -46,11 +46,11 @@ fbs <- function(X,Xcat,pik){
   
   
   for(k in 1:H){
-    # pik_tmp[Xcat == k] <- sampling::fastflightcube(cbind(pik[which(Xcat == k)],as.matrix(X[which(Xcat == k),])),
-    #                                                pik[Xcat == k],
+    # pik_tmp[strata == k] <- sampling::fastflightcube(cbind(pik[which(strata == k)],as.matrix(X[which(strata == k),])),
+    #                                                pik[strata == k],
     #                                                comment = FALSE)
-    pik_tmp[Xcat == k] <- ffphase(as.matrix(cbind(pik[which(Xcat == k)],as.matrix(X[which(Xcat == k),]))),
-                                                   pik[Xcat == k])
+    pik_tmp[strata == k] <- ffphase(as.matrix(cbind(pik[which(strata == k)],as.matrix(X[which(strata == k),]))),
+                                                   pik[strata == k])
     
   }
   sum(pik_tmp)
@@ -60,23 +60,25 @@ fbs <- function(X,Xcat,pik){
   ##----------------------------------------------------------------
   i <- which(pik_tmp > EPS & pik_tmp < (1-EPS))
   if(length(i) != 0){
-    Xnn <- disjMatrix(as.matrix(Xcat))
+    Xnn <- disj(strata)
     for(k in 1:H){
       # print(k)
       
-      i <- which(Xcat <= k & (pik_tmp > EPS & pik_tmp < (1-EPS)))
+      i <- which(strata <= k & (pik_tmp > EPS & pik_tmp < (1-EPS)))
       
-      # Xcat_tmp2 <- Xnn[i,1:k]
-      Xcat_tmp2 <- disjMatrix(as.matrix(Xcat[i,]))
-      Xcat_tmp2 <- Xcat_tmp2*pik_tmp[i]
+      # strata_tmp2 <- Xnn[i,1:k]
+      # strata_tmp2 <- disjMatrix(as.matrix(strata[i]))
+      
+      strata_tmp2 <- disj(strata[i])
+      strata_tmp2 <- strata_tmp2*pik_tmp[i]
       
       X_tmp <- as.matrix((X[i,]*pik_tmp[i]/pik[i]))
       
-      # pik_tmp[i] <- sampling::fastflightcube(as.matrix(cbind(X_tmp,Xcat_tmp2)),
+      # pik_tmp[i] <- sampling::fastflightcube(as.matrix(cbind(X_tmp,strata_tmp2)),
       #                                        pik_tmp[i],
       #                                        1,
       #                                        comment = FALSE)
-      pik_tmp[i] <- ffphase(as.matrix(cbind(X_tmp,Xcat_tmp2)),
+      pik_tmp[i] <- ffphase(as.matrix(cbind(X_tmp,strata_tmp2)),
                                              pik_tmp[i])
     }
   }
@@ -90,9 +92,9 @@ fbs <- function(X,Xcat,pik){
 
   
   if(length(i) != 0){
-  Xcat_tmp3 <- as.matrix(Xnn)
-  # Xcat_tmp3 <- as.matrix(Xnn[i,])
-  pik_tmp <- landingRM(as.matrix(cbind(Xcat_tmp3, X/pik)),
+  strata_tmp3 <- as.matrix(Xnn)
+  # strata_tmp3 <- as.matrix(Xnn[i,])
+  pik_tmp <- landingRM(as.matrix(cbind(strata_tmp3, X/pik)),
                           pik_tmp)
   # pik_tmp[i] <- landingRM(as.matrix(cbind(Xnn[i,], X[i,])),
   #                         pik_tmp[i],
