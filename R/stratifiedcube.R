25f2b92f4a9dea48cd25688ff20d463e1db3fdd2
JAUSLIN Raphaël <raphael.jauslin@unine.ch>
2022-11-28 16:22:36 +0100

    stratified_wo_landing

--- R/stratifiedcube.R
+++ R/stratifiedcube.R
@@ -55,17 +55,21 @@
 #' EPS = 1e-7
 #' 
 #' s <- stratifiedcube(X,strata,pik)
+#' test <- stratifiedcube_wo_landing(X,strata,pik)
 #' 
 #' t(X/pik)%*%s
+#' t(X/pik)%*%test
 #' t(X/pik)%*%pik
 #' 
 #' Xcat <- disj(strata)
 #' 
 #' t(Xcat)%*%s
+#' t(Xcat)%*%test
 #' t(Xcat)%*%pik
 #' 
 #' 
 #' # EXAMPLE WITH UNEQUAL INCLUSION PROBABILITES AND SUM IN EACH STRATA NOT INTEGER
+#' set.seed(3)
 #' N <- 100
 #' n <- 10
 #' X <- cbind(rgamma(N,4,25),rbinom(N,20,0.1),rlnorm(N,9,0.1),runif(N))
@@ -74,17 +78,22 @@
 #' pik <- runif(N)
 #' EPS = 1e-7
 #' tapply(pik,strata,sum)
+#' table(strata)
 #' 
 #' 
 #' s <- stratifiedcube(X,strata,pik,landing = TRUE)
+#' test <- stratifiedcube_wo_landing(X,strata,pik,landing = TRUE)
+#' 
 #' 
 #' t(X/pik)%*%s
+#' t(X/pik)%*%test
 #' t(X/pik)%*%pik
 #' 
 #' Xcat <- disj(strata)
 #' 
 #' t(Xcat)%*%s
 #' t(Xcat)%*%pik
+#' t(Xcat)%*%test
 #' 
 #' 
 stratifiedcube <- function(X,strata,pik,EPS = 1e-7,rand = TRUE,landing = TRUE){
@@ -273,3 +282,160 @@ stratifiedcube <- function(X,strata,pik,EPS = 1e-7,rand = TRUE,landing = TRUE){
   
 }
 
+
+
+#' @noRd
+#' @export
+stratifiedcube_wo_landing <- function(X,strata,pik,EPS = 1e-7,rand = TRUE,landing = TRUE){
+  
+  if(rand == TRUE){
+    strataInit <- strata
+    ## cleanstrata
+    a = sort(unique(strata))
+    b = 1:length(a)
+    names(b) = a
+    strata <- as.vector(b[as.character(strata)])
+    
+    ## RANDOMIZATION 
+    o <- order(strata)
+    # strata[o] # this order the vector
+    
+    o_split <- split(o, f = strata[o] ) # split and randomize in each category
+    for( i in 1:length(o_split)){
+      o_tmp <- sample(1:length(o_split[[i]]))
+      o_split[[i]] <- o_split[[i]][o_tmp]
+    }
+    o_out <- unlist(o_split[sample(1:length(o_split))]) # unlist and randomize each category
+    
+    XInit <- X
+    pikInit <- pik  
+    
+    X <- X[o_out,]
+    strata <- strata[o_out]
+    pik <- pik[o_out]
+  }
+  
+  ##----------------------------------------------------------------
+  ##                        Initialization                         -
+  ##----------------------------------------------------------------
+  # strata <- as.matrix(strata)
+  A = X/pik
+  pikstar <- pik
+  p = ncol(X)
+  nstrata <- length(unique(strata))
+  # TOT <- t(A)%*%pik
+  # XX <- MASS::ginv(t(A) %*% A)
+  
+  ##----------------------------------------------------------------
+  ##                  Flightphase on each strata                   -
+  ##----------------------------------------------------------------
+  
+  for(k in 1:nstrata){
+    pikstar[strata == k] <-ffphase(as.matrix(cbind(pikstar[strata == k],X[which(strata == k),])),
+                                   pikstar[strata == k])
+  }
+  
+  ###################### CHECK
+  # t(X/pik)%*%pikstar
+  # t(X/pik)%*%pik
+  # Xcat <- disj(strata)
+  # t(Xcat)%*%pik
+  # t(Xcat)%*%pikstar
+  
+  ##----------------------------------------------------------------
+  ##                Number of non 0-1 inclusion prob               -
+  ##----------------------------------------------------------------
+  
+  i <- which(pikstar > EPS & pikstar < (1-EPS))
+  i_size = length(i)
+  i_size
+  
+  
+  ##----------------------------------------------------------------
+  ##            flight phase with categorical variable             -
+  ##----------------------------------------------------------------
+  
+  if(i_size > 0 ){
+    while(i_size > 0){
+      
+      ##------ Remove unique category
+      uCat <- i[duplicated(strata[i]) | duplicated(strata[i], fromLast = TRUE)]
+      if(length(uCat) == 0){
+        break;
+      }
+      
+      ##------ Find B
+      A_tmp <- X[uCat,]/pik[uCat]
+      B <- findB(A_tmp,strata[uCat])
+      B <- cbind(B$X,B$Xcat)
+      ##------ onestep and check if null
+      tmp <-  onestep(B,pikstar[uCat[1:nrow(B)]],EPS)
+      if(is.null(tmp)){
+        break;
+      }else{
+        pikstar[uCat[1:nrow(B)]] <- tmp
+      }
+      
+      ##------ update i
+      
+      i <- which(pikstar > EPS & pikstar < (1-EPS))
+      i_size = length(i)
+      # print(sum(pikstar))
+    }
+    
+    
+    # ##----------------------------------------------------------------
+    # ##          end of flight phase on strata categories             -
+    # ##----------------------------------------------------------------
+    # Sometimes some stata does could not be balanced at the end and so we 
+    # drop some auxiliary variable such that we could have only one unit that
+    # are not put equal to 0 or 1 within each strata
+    #
+    
+    p <- ncol(X)
+    k = 1
+    while(length(uCat) != 0){
+      ##------ Find B
+      if(k == p){
+        B <- as.matrix(pikstar[uCat])/pikstar[uCat]
+      }else{
+        A_tmp <- X[uCat,1:(p-k)]/pik[uCat]
+        B <- findB(A_tmp,strata[uCat])
+        B <- cbind(B$X,B$Xcat)
+      }
+      
+      # print(pikstar[uCat[1:nrow(B)]])
+      tmp <-  onestep(B,pikstar[uCat[1:nrow(B)]],EPS)
+      # print(tmp)
+      if(!is.null(tmp)){
+        pikstar[uCat[1:nrow(B)]] <- tmp  
+      }
+      i <- which(pikstar > EPS & pikstar < (1-EPS))
+      i_size = length(i)
+      uCat <- i[duplicated(strata[i]) | duplicated(strata[i], fromLast = TRUE)]
+      k = k + 1
+    }
+  }
+  
+  #---------------- check
+  Xcat <- disjMatrix(as.matrix(strata))
+
+  print(t(X/pik)%*%pik)
+  print(t(X/pik)%*%pikstar)
+  print(t(Xcat)%*%pik)
+  print(t(Xcat)%*%pikstar)
+  print(length(i))
+  print(sum(pikstar))
+
+    
+  if(rand == TRUE){
+    out <- rep(0,length(pikstar))
+    # out[o_out[which(pikstar > (1-EPS))]] <- 1
+    out[o_out[which(pikstar > EPS)]] <- pikstar[which(pikstar > EPS)]
+  }else{
+    out <- round(pikstar,10)
+  }
+  return(out)
+  
+  
+}
